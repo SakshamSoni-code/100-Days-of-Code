7579542a962f7589e9b5b749b3836c787ca836f9
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-01-27 17:34:05 +0530

    Add files via upload

---
+++ Day-4/Maximising Vacations(Codechef).cpp
@@ -0,0 +1,97 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+void problem_solver()
+{
+    int n,k;
+    cin>>n>>k;
+    
+    string s;
+    cin>>s;
+    
+    int cnt=0,ans=0;
+    vector<int> temp;
+    bool b=false;
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]=='0'){
+            cnt++;
+            b=true;
+            if(i==n-1){
+               temp.push_back(cnt);
+            ans+=(cnt/k); 
+            }
+        }
+        else if(b==true){
+            temp.push_back(cnt);
+            ans+=(cnt/k);
+            cnt=0;
+        }
+    }
+ 
+    for(int i=1;i<temp.size();i++)
+    {
+        if((temp[i-1]%k + temp[i]%k + 1)>=k) {
+            ans++;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/

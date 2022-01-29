b6527e410cd74f42b02eea6b426ec22bcaa00456
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-01-29 13:33:10 +0530

    Add files via upload

---
+++ Day-6/C-Paint the array(Codeforces).cpp
@@ -0,0 +1,88 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+void problem_solver()
+{
+    int n;
+    cin>>n;
+    
+    vector<ll> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    
+    ll x=0,y=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(i%2==0) x=__gcd(x,v[i]);
+        else y=__gcd(y,v[i]);
+    }
+    
+    bool ok1=1,ok2=1;
+    
+    for(int i=0;i<n;i++){
+        if(i%2==0 && v[i]%y==0)ok1=0;
+        if(i%2!=0 && v[i]%x==0)ok2=0;
+    }
+    
+    if(ok2) cout<<x<<endl;
+    else if(ok1) cout<<y<<endl;
+    else cout<<0<<endl;
+  
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/

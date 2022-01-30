c12d2e6d6c1379161ca07188562bfd650551ea8c
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-01-30 16:18:41 +0530

    Add files via upload

---
+++ Day-7/B-Mirror in the String(Codeforces).cpp
@@ -0,0 +1,93 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+void problem_solver()
+{
+    int n;
+    cin>>n;
+    
+    string s;
+    cin>>s;
+    if(s[0]=='a'){
+        cout<<"aa"<<endl;
+        return;
+    }
+    if(s.size()==1){
+        s+=s;
+        cout<<s<<endl;
+        return;
+    }
+    string ans="";
+    bool f = (s[0] != s[1]);
+    for(int i=0;i<s.size();i++)
+    {
+        ans+=s[i];
+       // cout<<ans<<endl;
+        if(i<s.size()-1 && s[i]<s[i+1]) break;
+        if(i < s.size()-1 && s[i] == s[i+1] && f == false) break;
+        
+    }
+    //string t= reverse(ans.begin() , ans.end());
+    //ans+=t;
+    string t= ans;
+    reverse(t.begin(),t.end());
+    ans+=t;
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/

b9c06e6a3169e420966745e72495bac1a30046e7
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-02-04 18:10:33 +0530

    Add files via upload

---
+++ Day-12/Subarray with given sum(GFG).cpp
@@ -0,0 +1,38 @@
+ vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        int i=0,j=0;
+        long long sum=arr[0];
+        while(i<n){
+            if(sum==s){
+                return {i+1, j+1};
+            }
+            
+            else if(sum < s){
+                j++;
+                sum+=arr[j];
+            }
+            else{
+                sum-=arr[i];
+                i++;
+            }
+        }
+        return {-1};
+        
+        // Your code here
+        /*map<int,int> mp;
+        vector<int> ans;
+        mp[0]=0;
+        long long prefix_sum=0;
+        for(int i=0 ;i<n;i++)
+        {
+           prefix_sum+=arr[i];
+           if(mp.find(prefix_sum-s)!=mp.end()){
+               ans.push_back(mp[prefix_sum-s]+2);
+               ans.push_back(i+1);
+               break;
+           }
+           mp[prefix_sum]=i;
+
+        }
+        return ans;  */
+    }

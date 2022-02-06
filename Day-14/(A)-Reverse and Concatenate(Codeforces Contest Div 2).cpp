c03dd6caccde123c2b1112261ff2c1a9c83bc4b7
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-02-06 21:00:34 +0530

    Add files via upload

---
+++ Day-14/(A)-Reverse and Concatenate(Codeforces Contest Div 2).cpp
@@ -0,0 +1,105 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+#define check       cout<<checkedDude<<endl
+
+const ll mod = 1000000007; //10^9+7
+
+using namespace std;
+
+void problem_solver()
+{
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    
+    if(k==0){
+        cout<<1<<endl;
+        return;
+    }
+    
+    int i=0,j=n-1;
+    bool b=false;
+    
+    if(n==1){
+        cout<<1<<endl;
+        return;
+    }
+    
+    while(i<j){
+        if(s[i]!=s[j])
+        {
+            b=true;
+            break;
+        }
+        i++;
+        j--;
+    }
+    
+    if(b==false){
+        cout<<1<<endl;
+        return;
+    }
+    
+    if(k>=2){
+        cout<<2<<endl;
+        return;
+    }
+    else{
+        cout<<1<<endl;
+        return;
+    }
+    
+}
+
+int32_t main() {
+    
+    fast;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        problem_solver();
+    }
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/

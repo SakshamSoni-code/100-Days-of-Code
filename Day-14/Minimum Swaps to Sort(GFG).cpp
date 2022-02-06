a7f03c979ca2f27d6358fa2f0e503fe1c6002c3e
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-02-06 23:52:33 +0530

    Add files via upload

---
+++ Day-14/Minimum Swaps to Sort(GFG).cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums)
+	{
+	    // Code here
+	    unordered_map<int,int> mp;
+	    for(int i=0;i<nums.size();i++)
+	    {
+	        mp[nums[i]]=i;
+	    }
+	    sort(nums.begin() ,nums.end());
+	    int cnt=0;
+	    
+	    for(int i=0;i<nums.size();i++)
+	    {
+	        if(mp[nums[i]]!=i){
+	            swap(nums[i], nums[mp[nums[i]]]);
+	            cnt++;
+	            i--;
+	        }
+	    }
+	    return cnt;
+	}

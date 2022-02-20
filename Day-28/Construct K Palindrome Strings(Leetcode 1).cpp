2bff4c1e184918150da0fd63c53192755c03381b
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2022-02-20 10:05:06 +0530

    Add files via upload

---
+++ Day-28/Construct K Palindrome Strings(Leetcode 1).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        
+     
+        
+        if(k > s.size()) return false;
+        
+        unordered_map<char,int> mp;
+        
+        for(char c : s){
+            mp[c]++;
+        }
+        int cnt=0;
+        for(auto it : mp){
+            if(it.second % 2 !=0){
+                cnt++;
+            }
+        }
+        if(cnt > k) return false;
+        
+        return true;
+        
+    }
+};
